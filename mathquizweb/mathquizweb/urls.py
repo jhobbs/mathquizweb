91a2a008cad53fd34782b178aebde775e68981a0
Jason Hobbs <jason.hobbs@gmail.com>
2015-05-30 00:46:41 -0500

    Setup base templates/style, and users.

--- mathquizweb/mathquizweb/urls.py
+++ mathquizweb/mathquizweb/urls.py
@@ -6,6 +6,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'mathquizweb.views.home', name='home'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/register/$', 'mathquizweb.views.register'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- mathquizweb/mathquizweb/views.py
+++ mathquizweb/mathquizweb/views.py
@@ -5,6 +5,8 @@ from mathquiz.quiz import Quiz
 from mathquiz.questions import builtin_question_types
 from mathquiz.storage import get_current_user_data
 
+from mathquizweb.forms import UserForm
+
 defaultoptions = namedtuple('options', [])
 
 def home(request):
@@ -18,3 +20,29 @@ def home(request):
         'index.html',
         {'question': question},
         context_instance=context)
+
+def register(request):
+    context = RequestContext(request)
+
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+
+        if not user_form.is_valid():
+            return render_to_response(
+                'registration/register.html',
+                {'user_form': user_form, 'registered': registered},
+                context)
+
+        user = user_form.save()
+        user.set_password(user.password)
+        user.save()
+        registered = True
+    else:
+        user_form = UserForm()
+
+    return render_to_response(
+        'registration/register.html',
+        {'user_form': user_form, 'registered': registered},
+        context)
